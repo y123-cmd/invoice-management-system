7ced2d4bd536e1eeb19024be4e484e833bf0ac48
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2026-02-06 15:57:40 +0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/imbank/payments/corporate/corporateinvoicesystem/CorporateInvoiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imbank.payments.corporate.corporateinvoicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateInvoiceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateInvoiceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imbank/payments/corporate/corporateinvoicesystem/CorporateInvoiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imbank.payments.corporate.corporateinvoicesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateInvoiceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
