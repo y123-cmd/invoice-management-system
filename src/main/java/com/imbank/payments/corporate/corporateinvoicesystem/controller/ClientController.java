1ebbc0a704eac633955e5b65180360bd74d4613a
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2026-02-11 13:40:57 +0300

    test

--- src/main/java/com/imbank/payments/corporate/corporateinvoicesystem/controller/ClientController.java
+++ src/main/java/com/imbank/payments/corporate/corporateinvoicesystem/controller/ClientController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// controller
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
