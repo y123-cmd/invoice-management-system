2d9fd8d41a1fffce0f86c5891d525ef83f88c4f6
Yvonne Bosire <yvonnebosire24@gmail.com>
2026-03-05 13:22:00 +0300

    add springsecurity with jwtUtil to generate tokens when users login extratc email from token and validate tokens on evry request,customDetailUserService to load users from database by email,jwtAuthenticationFilter to accept incoming requests token is valid and securityConfig to config security rules login and register are public and evrything else requires a valid token csrf is disabled we use jwt

---
+++ src/main/java/com/imbank/payments/corporate/corporateinvoicesystem/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.imbank.payments.corporate.corporateinvoicesystem.security;
+
+import com.imbank.payments.corporate.corporateinvoicesystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+        throws UsernameNotFoundException{
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "User not found with email: " + email
+                ));
+    }
+}
---
+++ src/main/java/com/imbank/payments/corporate/corporateinvoicesystem/security/JwtUtil.java
@@ -0,0 +1,54 @@
+package com.imbank.payments.corporate.corporateinvoicesystem.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+}
